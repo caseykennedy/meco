8621fb3fc0e4b7d92f041d84b1c52e7aac4c07f7
Casey K <tehcnicolour@gmail.com>
2020-03-22 19:12:49 -0700

    hidden fields

--- src/components/ReservationForm/ReservationForm.tsx
+++ src/components/ReservationForm/ReservationForm.tsx
@@ -45,6 +45,12 @@ const ReservationForm: React.SFC<Props> = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
+        <input type="hidden" name="bot-field" />
+        <input
+          type="hidden"
+          name="meco-reservations"
+          value="meco-reservation-form"
+        />
         <fieldset>
           <Box width={1} className="form-group">
             <Box width={[1, 1, 1 / 2]} className="form-group__box">
@@ -171,26 +177,6 @@ const ReservationForm: React.SFC<Props> = () => {
           Submit
         </button>
       </S.Form>
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
     </S.Container>
   )
 }
