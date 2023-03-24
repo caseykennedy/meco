9898e67572e272459295816aaf3c6f47a60d229f
Casey K <tehcnicolour@gmail.com>
2023-03-24 11:21:55 -0700

    installs robots plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -114,14 +114,14 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sitemap',
-    // {
-    //   resolve: 'gatsby-plugin-robots-txt',
-    //   options: {
-    //     host: config.siteUrl,
-    //     sitemap: `${config.siteUrl}/sitemap.xml`,
-    //     policy: [{ userAgent: '*', allow: '/' }]
-    //   }
-    // },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: config.siteUrl,
+        sitemap: `${config.siteUrl}/sitemap.xml`,
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
