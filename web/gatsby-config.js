9595642992735997ee99198c23b12d85e2f82573
Casey K <tehcnicolour@gmail.com>
2023-03-24 11:20:14 -0700

    updated breadcrumb list

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -32,6 +32,29 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-image',
     'gatsby-transformer-yaml',
+    // {
+    //   resolve: 'gatsby-plugin-google-tagmanager',
+    //   options: {
+    //     id: config.googleTagID,
+
+    //     // Include GTM in development.
+    //     //
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
+
+    //     // datalayer to be set before GTM is loaded
+    //     // should be an object or a function that is executed in the browser
+    //     //
+    //     // Defaults to null
+    //     defaultDataLayer: { platform: 'gatsby' },
+
+    //     // Name of the event that is triggered
+    //     // on every Gatsby route change.
+    //     //
+    //     // Defaults to gatsby-route-change
+    //     routeChangeEventName: 'gatsby-route-change',
+    //   },
+    // },
     {
       resolve: 'gatsby-source-sanity',
       options: {
